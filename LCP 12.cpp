f4a6033b5d78e2e4eb42605b5291573a95df43ef
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-04-17 23:48:21 +1000

    删除dbg

--- LCP 12.cpp
+++ LCP 12.cpp
@@ -7,8 +7,6 @@
 
 using namespace std;
 
-#define debug // 定义调试模式
-
 class Solution {
 public:
     int n = 0;
@@ -20,29 +18,12 @@ public:
         int mid = 0;
         while(l + 1 != r){
             mid = floor((l + r) / 2);
-            #ifdef debug
-            cout << "mid: " << mid << endl;
-            cout << "l: " << l << endl;
-            cout << "r: " << r << endl;
-            #endif
             if(!check(time, mDays, mid)){//if判断 应当表示蓝色区，即m不足时
                 l = mid;
-                #ifdef debug
-                cout << "l = mid = " << mid << endl;
-                #endif
             }else{
                 r = mid;
-                #ifdef debug
-                cout << "r = mid = " << mid << endl;
-                #endif
             }
-            #ifdef debug
-            cout << endl;
-            #endif
         }
-        #ifdef debug
-        cout << "return r;"<< r << "now l: " << l << "now r: " << r << endl;
-        #endif
         return r;
     }
     
@@ -54,37 +35,13 @@ public:
         for(int i = 0; i < n; ++i){
             pq.push_back(time[i]);
             dailySum += time[i];
-            #ifdef debug
-            cout << "Day" << days << " - try question: " << time[i] << endl;
-            cout << "dailySum: " << dailySum << endl;
-            #endif
             if(dailySum > testSum){
-                #ifdef debug
-                cout << "Go beyond MaxTime: " << testSum << endl;
-                #endif
                 if(exemption){
-                    #ifdef debug
-                    cout << "dailySum: " << dailySum << endl;
-                    for(auto& t : pq){cout << "pq: " << t << " ";}
-                    cout<<endl;
-                    #endif
                     sort(pq.begin(), pq.end(), std::less<>());
-                    #ifdef debug
-                    for(auto& t : pq){cout << "pq: " << t << " ";}
-                    cout<<endl;
-                    #endif
                     exemption = false;
                     dailySum -= pq.back();
-                    #ifdef debug
-                    cout << "Day" << days << " - use exemption for time: " << pq.back() << endl;
-                    cout << "dailySum - pq.top() = " << dailySum << endl;
-                    #endif
                 }else{
-                    #ifdef debug
-                    cout << "No exemption, go to next day" << endl;
-                    #endif
                     dailySum = 0;
-                    // pq.clear();
                     pq.clear();
                     days ++;
                     exemption = true;
@@ -95,9 +52,6 @@ public:
         }
         //双重否定表肯定，应返回恰好或有余 消耗days少于等于被测t
         bool res = days <= mDays;
-        #ifdef debug
-        cout << mDays << " Days enough for MaxTime: " << testSum << " ? " << (res ? "Yes" : "No") << endl;
-        #endif
         return res;
     }
 };
@@ -109,7 +63,6 @@ int main() {
     int expected = 7;
     
     int result = sol.minTime(time, m);
-    //乱码需要解决
     
     cout << "test res: " << result << endl;
     cout << "expect " << expected << endl;
